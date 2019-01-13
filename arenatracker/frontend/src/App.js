7d66e848fa814ca4f805374b2a745b3076cbed70
Mcorupe <40810011+Mcorupe@users.noreply.github.com>
2019-01-13 14:39:51 -0700

    Added Dependancies, Reworked App.js, Reformated src/components, added themeprovider

--- arenatracker/frontend/src/App.js
+++ arenatracker/frontend/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import {Router, Route} from "react-router-dom";
 import logo from './logo.svg';
+import {ThemeContext, themes} from "./ThemeProvider";
+import Login from "./Views/Login";
+import SignUp from "./Views/SignUp";
+import Home from "./Views/Home/Home";
 import './App.css';
+import "./Components/Header/Header.css"
 
 class App extends Component {
 constructor() {
@@ -8,13 +14,61 @@ constructor() {
 
   this.state = {
     user: {},
-    form: ""
-       
-  }
+    form: "",
+    theme: themes.default
+           
+  };
+
+  this.toggleTheme = this.toggleTheme.bind(this);
+  this.handleFilter = this.handleFilter.bind(this);
+  this.submitForm = this.submitForm.bind(this);
+  this.getForm = this.getForm.bind(this);
+
 }
 
+toggleTheme() {
+  this.setState(state => ({
+    theme: state.theme === themes.horde ? themes.ally : themes.default
+  }))
+}
 
-  
+handleFilter(e) {
+  const {value} = e.target;
+  this.setState({
+    filter: value
+  })
+}
+
+ 
+
+
+render() {
+  const { TheForm, user, filter, theme, Header} = this.state;
+  const themeChange = {
+    theme,
+    toggleTheme: this.toggleTheme
+  };
+  return(
+    <ThemeContext.Provider value={themeChange}>
+      <Router>
+        <>
+          <Route exact path="/" 
+            render={() => (
+              <Home 
+                user={user}
+                filter={filter}
+                handleFilter={this.handleFilter}
+                submitForm={this.submitForm}
+              />
+            )}
+          />
+          <Route exact path="/Login" Component={Login} />
+          <Route exact path="/Signup" Component={SignUp} />
+          </>
+      </Router>
+    </ThemeContext.Provider>
+    );
+  }
 }
 
 export default App;
